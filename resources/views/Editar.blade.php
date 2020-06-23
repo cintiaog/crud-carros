f0823a47557794f245a1006eae7c1b2cfe94bf5e
Cintia <cintiaog@hotmail.com>
2020-06-23 18:15:38 -0300

    Ediçao de imagem

--- resources/views/Editar.blade.php
+++ resources/views/Editar.blade.php
@@ -17,7 +17,7 @@
     </div>
     @endforeach   
     @endif
-<form name="formCad" id="formCad" method="POST" action="{{route('update', $carros->id)}}">
+<form name="formCad" id="formCad" method="POST" action="{{route('update', $carros->id)}}" enctype="multipart/form-data">
      @csrf
     
 <input class="form-control" type="text" name="marca" id="marca" placeholder="Marca"  value="{{$carros->marca}}"><br>
@@ -25,7 +25,7 @@
 <input class="form-control" type="text" name="ano" id="ano" placeholder="Ano:" value="{{$carros->ano}}"><br>
     <input class="form-control" type="text" name="km" id="km" placeholder="KM:" value="{{$carros->km}}"><br>
     <input class="form-control" type="text" name="price" id="price" placeholder="Preço:"  value="{{$carros->price}}"><br>
-    
+    <input type="file" class="form-control-file" id="enviarImagem" name="image" value="{{$carros->image}}">
     <input class="btn btn-success" type="submit" value="Atualizar">
     <a href ="{{route('home')}}">
         <button class="btn btn-primary">Voltar</button>
